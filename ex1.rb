e78c24bc74eb274ae2f41e0899c449505c047b2c
Chrisl154 <christian.massie@gmail.com>
2017-02-20 15:14:28 -0700

    Removal of divison

--- ex1.rb
+++ ex1.rb
@@ -85,21 +85,25 @@
 #and then put the variable you want inside the {} characters. This tells Ruby, "Hey, this string
 #needs to be formatted. Put these variables in there."
 
-my_name = 'Christian Massie'
-my_age = 25 # written in 2017 ;)
-my_height = 66 # inches
-my_weight = 138 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Black'
+# my_name = 'Christian Massie'
+# my_age = 25.0 # written in 2017 ;)
+# my_height = 66.0 # inches
+# my_weight = 138.0 # lbs
+# my_eyes = 'Blue'
+# my_teeth = 'White'
+# my_hair = 'Black'
 
 
 
-puts "Lets talk about #{my_name}"
-puts "He's #{my_height / 12} feet tall."
-puts "He's #{my_weight} pounds heavy."
-puts "Actually...Thats really not heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the soda."
+# puts "Lets talk about #{my_name}"
+# puts "He's #{my_height} inches tall."
+# puts "He's #{my_weight} pounds heavy."
+# puts "Actually...Thats really not heavy."
+# puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+# puts "His teeth are usually #{my_teeth} depending on the soda."
 
-puts "If I add my age #{my_age}, my height #{my_height}, and my weight #{my_weight} I get #{my_age + my_height + my_weight}."
+# puts "If I add my age #{my_age}, my height #{my_height}, and my weight #{my_weight} I get #{my_age + my_height + my_weight}."
+
+# End of ex5
+
+# Beginning of ex6 - Strings and Text
